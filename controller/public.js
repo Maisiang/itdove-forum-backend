9a0ec282dd42a3936b71121a8fed86da2806c5da
maisiang <maisiang001@gmail.com>
2023-05-12 23:58:47 +0800

    增加文章分頁

--- controller/public.js
+++ controller/public.js
@@ -4,19 +4,24 @@ const mysql = require('../lib/mysql');
 exports.getKanban = (request,response) => {
     const sql = 'SELECT * FROM KANBAN';
     mysql.connection.query(sql, (error, results) => {
-        /*if (error) {
-            return Error(error, response);
-        }*/
+        /*if (error) {return Error(error, response);}*/
         response.json(results);
     });
 }
 // 取得文章
 exports.getArticle = (request,response) => {
-    const sql = 'SELECT * FROM ARTICLE WHERE KANBAN_ID = ?';
-    mysql.connection.query(sql, [request.params.kanbanID], (error, results) => {
-        /*if (error) {
-            return Error(error, response);
-        }*/
+    console.log(request.query.page);
+    let params = [];
+    let sql = "SELECT ARTICLE.* \
+                FROM  ARTICLE, KANBAN \
+                WHERE ARTICLE.KANBAN_ID = KANBAN.KANBAN_ID ";
+    if(request.params.kanbanID != "all"){
+        sql += "AND KANBAN.NAME_EN = ? ";
+        params.push(request.params.kanbanID);
+    }
+    sql += "LIMIT " + request.query.page * 10 + ", 10";
+    mysql.connection.query(sql, params, (error, results) => {
+        console.log('結果：',results);
         response.json(results);
     });
 }
