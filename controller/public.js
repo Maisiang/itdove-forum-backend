f9aafe6111944f8aec2b1419c7039d059f433083
maisiang <maisiang001@gmail.com>
2023-05-21 20:32:52 +0800

    完成登入、註冊基本功能、使用bcrypt加密密碼

--- controller/public.js
+++ controller/public.js
@@ -1,5 +1,8 @@
-const postgreSQL = require('../lib/postgreSQL');
-const jwt = require('jsonwebtoken');
+const jwt       = require('jsonwebtoken');
+const bcrypt    = require('bcrypt');
+
+const postgreSQL    = require('../lib/postgreSQL');
+const common        = require('../lib/common');
 
 
 // 取得看板
@@ -27,26 +30,61 @@ exports.getArticle = async (request,response) => {
 
 // 用戶登入
 exports.login = async (request,response) => {
-    if(request.cookies!=undefined){
-        console.log("TOKEN: " ,request.cookies.token);
-        jwt.verify(request.cookies.token, "test", (err, decoded) => {
-            if (err) {
-                console.log(err);
-            } else
-            console.log(decoded)
+    // 帳號密碼驗證
+    const sql = 'SELECT * FROM "USER" WHERE USERNAME = $1';
+    const result = await postgreSQL.client.query(sql,[request.body.username]);
+    if(result.rows.length > 0){
+        bcrypt.compare(request.body.password, result.rows[0].password, function(error, compareResult) {
+            if (error) {
+                Error(error,response);
+            } else if (compareResult) {
+                // 產生 Token
+                const token = jwt.sign(
+                    { username: result.rows[0].username }, 
+                    common.secretKey
+                );
+                // 將 JWT 儲存在 Cookie
+                response.cookie('token', token, { httpOnly: true });
+                response.json({ isSuccess: true });
+                return;
+            } else {
+                response.json({ isSuccess: false });
+            }
+        });
+    } else {
+        response.json({ isSuccess: false });
+    }
+}
+
+// 用戶註冊
+exports.register = async (request,response) => {
+    // 檢查帳號或信箱是否重複
+    let sql = 'SELECT * FROM "USER" WHERE USERNAME = $1 OR EMAIL = $2'
+    let params = [
+        request.body.username,
+        request.body.email,
+    ];
+    const result = await postgreSQL.client.query(sql,params);
+    if(result.rows.length > 0){
+        response.json({ isSuccess: false });
+    } else {
+        // 將密碼加密
+        bcrypt.hash(request.body.password, common.saltRounds, async function(error, hash) {
+            if (error) {
+                Error(error,response);
+            } else {
+                // 新增用戶到資料庫
+                sql = 'INSERT INTO "USER" (USERNAME, PASSWORD, EMAIL) VALUES ($1, $2, $3)';
+                params = [
+                    request.body.username,
+                    hash,
+                    request.body.email
+                ];
+                await postgreSQL.client.query(sql,params)
+                response.json({ isSuccess: true });
+            }
         });
     }
-
-    console.log('用戶登入：',request.body);
-    const token = jwt.sign(
-        { 
-            username: request.body.username 
-        }, 
-        "test"
-    );
-    // 將 JWT 儲存在 Cookie
-    response.cookie('token', token, { httpOnly: false });
-    response.json({ isSuccess: true });
 }
 
 // 錯誤處理
--- routes/public.js
+++ routes/public.js
@@ -8,6 +8,7 @@ const public = require('../controller/public');
 router.get('/kanban'            , public.getKanban);    // 取得看板
 router.get('/article/:kanbanID' , public.getArticle);   // 取得文章
 router.post('/login'            , public.login);        // 用戶登入
+router.post('/register'         , public.register);     // 用戶註冊
 
 
 
